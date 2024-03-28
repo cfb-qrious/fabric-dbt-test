15dec6b8bc67ae8bc2bf1a4f1e159b9e90fe96ce
cfb-qrious <christopher.beeston@qrious.co.nz>
2024-03-28 13:08:09 +1300

    Removed pakcages

--- models/gold/customers_d.sql
+++ models/gold/customers_d.sql
@@ -4,11 +4,10 @@ with customers as (
 )
 
 select
-    {{ dbt_utils.generate_surrogate_key(['customer_name']) }} as customer_id
-    , customer_name
+    customer_name
     , phone_number
     , birth_year
     , has_club_membership
     , count(stores_visited) as stores_visited_cnt
 from customers
-group by 1,2,3,4,5
+group by 1,2,3,4
