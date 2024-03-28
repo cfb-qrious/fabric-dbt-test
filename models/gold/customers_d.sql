8eaa6cd64969127985c4e0023ada6a88f46ceab9
cfb-qrious <christopher.beeston@qrious.co.nz>
2024-03-28 16:18:49 +1300

    Added use of a library

--- models/gold/customers_d.sql
+++ models/gold/customers_d.sql
@@ -4,10 +4,11 @@ with customers as (
 )
 
 select
-    customer_name
+    {{ dbt_utils.generate_surrogate_key(['customer_name']) }} as customer_id
+    , customer_name
     , phone_number
     , birth_year
     , has_club_membership
     , count(stores_visited) as stores_visited_cnt
 from customers
-group by 1,2,3,4
+group by 1,2,3,4,5
