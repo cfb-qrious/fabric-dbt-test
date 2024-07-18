1a2e4f7fce0c3acb4d839e369db4a5836ad04bd3
cfb-qrious <94415387+cfb-qrious@users.noreply.github.com>
2024-07-19 07:08:44 +1200

    Update silver_customers.sql

--- models/silver/silver_customers.sql
+++ models/silver/silver_customers.sql
@@ -1,27 +1,3 @@
-with source as (
-    
-    {#-
-    Normally we would select from the table here, but we are using seeds to load
-    our data in this project
-    #}
-    select * from {{ source('sales_db','customers_raw') }}
-
-)
-
-, final as (
-    select 
-        CustomerName as customer_name
-        , Phone as phone_number
-        , cast(BirthYear as int) as birth_year
-        , case
-            hasClubMembership
-            when "Yes" then 1
-            when "No" then 0
-            else null
-        end as has_club_membership
-        , StoresVisited as stores_visited
-    from source
-)
-
-select *
-from final
+select '1' as _id
+union 
+select '2' as _id
